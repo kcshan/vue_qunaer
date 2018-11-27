e57f0a5d19201858ca1dd1eba6d2604403bc92f2
孔垂善 <cshan.kong@gmail.com>
2018-11-27 21:20:15 +0800

    feat: init project

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="home">
+      {{msg}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Home'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home{
+  font-size: 20px;
+}
+</style>
